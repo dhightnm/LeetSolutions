2bfcced9a8f4f9d1035594a30c3a70f1e2126c02
Devin Hight <dhightnm@gmail.com>
2021-09-08 10:42:56 -0600

    Added hash solution for twoSum

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -3,16 +3,35 @@
  * @param {number} target
  * @return {number[]}
  */
+
+/* Brute Force Attempt
 var twoSum = function(nums, target) {
     
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
-                return [i,j];
-                
+                return [i,j]; 
             }
         }
-        
     }
-    
 };
+*/
+
+// Using Hash 
+
+var twoSum = function(nums, target) {
+  const hash = {};
+    
+  nums.forEach((item, index) => {
+    hash[item] = index;
+  }); 
+    
+  for (let index = 0; index < nums.length; index++){
+    const comp = target - nums[index];
+        
+    if (hash[comp] !== undefined && hash[comp] !== index) {
+      return [index, hash[comp]];
+    }
+  }
+};
+
