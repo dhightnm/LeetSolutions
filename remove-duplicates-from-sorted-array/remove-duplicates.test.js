8be93afdd763e9368401a5415f27d7bd43464335
Devin Hight <dhightnm@gmail.com>
2021-09-22 21:49:32 -0700

    Add tests for reverse-integer

--- remove-duplicates-from-sorted-array/remove-duplicates.test.js
+++ remove-duplicates-from-sorted-array/remove-duplicates.test.js
@@ -4,4 +4,6 @@ test('Should remove duplicates from an array', () => {
     expect(removeDuplicates([2, 2, 3, 4, 5, 5])).toEqual(4);
 });
 
-
+test('Should remove duplicates from an array', () => {
+    expect(removeDuplicates([2, 2, 3, 4, 5, 5, 6, 6, 7, 7])).toEqual(6);
+});
